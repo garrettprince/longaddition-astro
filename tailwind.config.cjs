edb2f449963a43e8462f946eeee6a99c45caeeac
garrettprince <garrettjamesprince@gmail.com>
2023-01-31 17:13:31 -0800

    Basic nav layout

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "cta-gradient-1": "#FF2929",
+        "cta-gradient-2": "#E205FF",
+      },
+    },
+  },
+  plugins: [],
+};
